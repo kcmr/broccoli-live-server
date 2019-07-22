021b6fcfe3c390a1bb9e9fdbe0ad7ddda9384438
Kus Cámara <kcmr@users.noreply.github.com>
2019-07-22 19:17:04 +0200

    test: starts a live server with custom options

--- tests/index.js
+++ tests/index.js
@@ -2,16 +2,25 @@
 
 const { createBuilder, createTempDir } = require('broccoli-test-helper');
 const sinon = require('sinon');
-const assert = require('assert');
+const { assert } = require('chai');
 const liveServer = require('live-server');
 const Plugin = require('../index');
 
 describe('broccoli-live-server', () => {
+  let liveServerStub;
+
+  beforeEach(() => {
+    liveServerStub = sinon.stub(liveServer, 'start');
+  });
+
+  afterEach(() => {
+    liveServerStub.restore();
+  });
+
   it('starts a live server with default options', async() => {
     const input = await createTempDir();
     const subject = new Plugin(input.path());
     const output = createBuilder(subject);
-    const liveServerStub = sinon.stub(liveServer, 'start');
 
     await output.build();
 
@@ -24,13 +33,21 @@ describe('broccoli-live-server', () => {
       root: input.path()
     });
 
-    // console.log(liveServerStub.getCall().args);
-    console.log(liveServerStub.getCall(0).args);
-
     assert.ok(liveServerStub.calledWith(expectedArguments));
   });
 
-  it.skip('starts a live server with custom options');
+  it('starts a live server with custom options', async() => {
+    const input = await createTempDir();
+    const customOptions = { foo: 'bar' };
+    const subject = new Plugin(input.path(), customOptions);
+    const output = createBuilder(subject);
+
+    await output.build();
+
+    const callArguments = liveServerStub.getCall(0).args[0];
+
+    assert.include(callArguments, customOptions);
+  });
 
   it.skip('does not start a new server if there is one running');
 });
